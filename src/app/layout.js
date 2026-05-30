4473d7eccaac57d63af95bb94b17bcd08c4acaae
sou963 <souravbosu844@gmail.com>
2026-05-30 11:44:07 +0600

    Update project

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,14 @@ import Navbar from "./component/Navbar";
 import Footer from "./component/footer";
 import "./globals.css";
 
+export const metadata = {
+  title: "MovieApp",
+  description: "Watch trending and popular movies",
+  icons: {
+    icon: "/logo.png",
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
